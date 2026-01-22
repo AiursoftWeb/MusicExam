c0dbb82324cada1b09ee39815ba92dda68e6b53f
Aimer Neige <aimer.neige.soft@gmail.com>
2026-01-22 13:52:09 +0000

    fix sing

--- src/Aiursoft.MusicExam/Models/DataTransferModels/QuestionDto.cs
+++ src/Aiursoft.MusicExam/Models/DataTransferModels/QuestionDto.cs
@@ -6,19 +6,19 @@ public class QuestionDto
 {
     [JsonPropertyName("question")]
     public required string Question { get; set; }
-    
+
     [JsonPropertyName("options")]
     public List<OptionDto> Options { get; set; } = new();
-    
+
     [JsonPropertyName("correctAnswer")]
-    public required string CorrectAnswer { get; set; }
+    public string? CorrectAnswer { get; set; }
 
     [JsonPropertyName("explanation")]
     public string? Explanation { get; set; }
 
     [JsonPropertyName("local_audios")]
     public List<string> LocalAudios { get; set; } = new();
-    
+
     [JsonPropertyName("local_images")]
     public List<string> LocalImages { get; set; } = new();
 }
